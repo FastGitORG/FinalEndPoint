1bd49f81d6f086b20bd4c9198f7643b9b8394abd
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2021-07-10 02:53:11 +0100

    optimise code

--- src/entry.go
+++ src/entry.go
@@ -8,33 +8,33 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		if r.RequestURI == "/" {
-			w.Write([]byte("OK by FinalEndPoint"))
-			return
-		}
 
-		if strings.HasPrefix(r.RequestURI, "/w2766/") {
-			http.Error(w, "402 - You NEED pay!", http.StatusPaymentRequired)
-			return
-		}
-
-		if isLog {
-			log.Println(r.Host + r.RequestURI)
-		}
-
-		url, ok := convFastGitToCloudFlareWorker(r.Host, r.RequestURI)
-		if ok {
-			http.Redirect(w, r, url, 301)
-		} else {
-			http.Error(w, "503 Bad Request - Not Allowed Url", http.StatusServiceUnavailable)
-		}
+	if r.RequestURI == "/" {
+		w.Write([]byte("OK by FinalEndPoint"))
 		return
-	default:
+	}
+
+	if r.Method != "GET" {
 		http.Error(w, "503 Bad Request - Not Allowed Method", http.StatusServiceUnavailable)
 		return
 	}
+
+	if strings.HasPrefix(r.RequestURI, "/w2766/") {
+		http.Error(w, "402 - You NEED pay!", http.StatusPaymentRequired)
+		return
+	}
+
+	if isLog {
+		log.Println(r.Host + r.RequestURI)
+	}
+
+	url, ok := convFastGitToCloudFlareWorker(r.Host, r.RequestURI)
+	if ok {
+		http.Redirect(w, r, url, 301)
+	} else {
+		http.Error(w, "503 Bad Request - Not Allowed Url", http.StatusServiceUnavailable)
+	}
+	return
 }
 
 func main() {
